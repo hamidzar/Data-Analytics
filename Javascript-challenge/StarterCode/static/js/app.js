191e5da00e83f41a36dc8add62c03b8596cd021f
hamidzar <hamid.zarringhalam@gmail.com>
2021-01-10 21:04:32 -0500

    Update app.js

--- Javascript-challenge/StarterCode/static/js/app.js
+++ Javascript-challenge/StarterCode/static/js/app.js
@@ -18,43 +18,24 @@ function runEnter() {
   var inputValue = inputElement.property("value");
   console.log(inputValue);
   console.log(tableData);
-  var filteredData = tableData.filter(person => person.datetime === inputValue);
-  console.log(filteredData);
+  //var filteredData = tableData.filter(info => info.datetime == inputValue);
+  ///console.log(filteredData);
   // BONUS: Calculate summary statistics for the age field of the filtered data
   // First, create an array with just the age values
-  var cities = filteredData.map(person => person.city);
-  // Next, use math.js to calculate the mean, median, mode, var, and std of the ages
-  
-  //var mean = math.mean(ages);
-  //var median = math.median(ages);
-  //var mode = math.mode(ages);
-  //var variance = math.var(ages);
-  //var standardDeviation = math.std(ages);
-
-  // Then, select the unordered list element by class name
-
-  ///var list = d3.select(".table table-striped");
-
-  // remove any children from the list to
-
-  ///list.html("");
-
-  // append stats to the list
-
-  ///list.append("li").text(`City: ${cities}`);
-
-  //list.append("li").text(`Median: ${median}`);
-  //list.append("li").text(`Mode: ${mode}`);
-  //list.append("li").text(`Variance: ${variance}`);
-  //list.append("li").text(`Standard Deviation: ${standardDeviation}`);
-
-  data.forEach((filteredData) => {
+  //var cities = filteredData.map(info => info.city);
+  var filterData = tableData.filter(info => info.datetime == inputValue);   
+data.forEach((tableData) => {
+   
+    if (tableData.datetime == inputValue){
+    
     var row = tbody.append("tr");
-    Object.entries(filteredData).forEach(([key, value]) => {
-      var cell = row.append("td");
-      cell.text(value);
+    Object.entries(tableData).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
     });
-  }); 
+  };
+});
+ 
 
 };
 
