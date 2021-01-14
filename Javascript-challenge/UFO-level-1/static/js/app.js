184d27a0d720f750ee7eae188eb4221d09aa6cee
hamidzar <hamid.zarringhalam@gmail.com>
2021-01-14 17:35:10 -0500

    Javascript Challenge

--- Javascript-challenge/UFO-level-1/static/js/app.js
+++ Javascript-challenge/UFO-level-1/static/js/app.js
@@ -14,15 +14,11 @@ function runEnter() {
   d3.event.preventDefault();
   // Select the input element and get the raw HTML node
   var inputElement = d3.select("#datetime");
-  var inputElement_city = d3.select("#city");
-  var inputElement_state = d3.select("#state");
+ 
   // Get the value property of the input element
-  var inputValue_date = inputElement.property("value");
-  var inputValue_city = inputElement_city.property("value");
-  var inputValue_state = inputElement_state.property("value");
-  console.log(inputValue_date);
-  console.log(inputValue_city);
-  console.log(inputValue_state);
+  var inputValue = inputElement.property("value");
+ 
+  console.log(inputValue);
   console.log(tableData);
   //var filteredData = tableData.filter(info => info.datetime == inputValue);
   ///console.log(filteredData);
@@ -30,87 +26,16 @@ function runEnter() {
   // First, create an array with just the age values
   //var cities = filteredData.map(info => info.city);
   tbody.html("")
-  ///var filterData = tableData.filter(info => info.datetime == inputValue_date);   
+  var filterData = tableData.filter(info => info.datetime == inputValue);   
 data.forEach((tableData) => {
-
-  if ((inputValue_date !== "") && (inputValue_state == "") && (inputValue_city == "")){
-    if ((tableData.datetime == inputValue_date)){
-        var row = tbody.append("tr");
-        Object.entries(tableData).forEach(([key, value]) => {
-        var cell = row.append("td");
-        cell.text(value);
-      });
-    };
-  };
-
-
-  if ((inputValue_date == "") && (inputValue_state !== "") && (inputValue_city == "")){
-    if ((tableData.state == inputValue_state)){
-        var row = tbody.append("tr");
-        Object.entries(tableData).forEach(([key, value]) => {
-        var cell = row.append("td");
-        cell.text(value);
-      });
-    };
-  };
-
-
-  if ((inputValue_date == "") && (inputValue_state == "") && (inputValue_city !== "")){
-    if ((tableData.city == inputValue_city)){
-        var row = tbody.append("tr");
-        Object.entries(tableData).forEach(([key, value]) => {
-        var cell = row.append("td");
-        cell.text(value);
-      });
-    };
-  };
-
-    if ((inputValue_date !== "") && (inputValue_state !== "") && (inputValue_city == "")){
-        if ((tableData.datetime == inputValue_date) && (tableData.state == inputValue_state)){
+   
+        if ((tableData.datetime == inputValue)){
             var row = tbody.append("tr");
             Object.entries(tableData).forEach(([key, value]) => {
             var cell = row.append("td");
             cell.text(value);
     });
   };
-};
-
-if ((inputValue_date !== "") && (inputValue_state == "") && (inputValue_city !== "")){
-  if ((tableData.datetime == inputValue_date) && (tableData.city == inputValue_city)){
-      var row = tbody.append("tr");
-      Object.entries(tableData).forEach(([key, value]) => {
-      var cell = row.append("td");
-      cell.text(value);
-    });
-  };
-};
-
-
-if ((inputValue_date == "") && (inputValue_state !== "") && (inputValue_city !== "")){
-  if ((tableData.state == inputValue_state) && (tableData.city == inputValue_city)){
-      var row = tbody.append("tr");
-      Object.entries(tableData).forEach(([key, value]) => {
-      var cell = row.append("td");
-      cell.text(value);
-    });
-  };
-};
-
-
-
-    if ((inputValue_date !== "") && (inputValue_state !== "") && (inputValue_city !== "")){
-      if ((tableData.datetime == inputValue_date) && (tableData.state == inputValue_state) && (tableData.city == inputValue_city)){
-          var row = tbody.append("tr");
-          Object.entries(tableData).forEach(([key, value]) => {
-          var cell = row.append("td");
-          cell.text(value);
-    });
-  };
-};
-
-
-
-
 
 });
  
