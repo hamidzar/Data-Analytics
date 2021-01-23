2af1a8a90e05af38fccf179939257b5be0ce637d
hamidzar <hamid.zarringhalam@gmail.com>
2021-01-23 16:57:49 -0500

    Update app.js

--- Plotly-Challenge/StarterCode/static/js/app.js
+++ Plotly-Challenge/StarterCode/static/js/app.js
@@ -1,9 +1,19 @@
 function megaData(sample){
   d3.json("samples.json").then((data) => {
       var metadata = data.metadata;
-      var filteredData = metadata.filter(d =>d.id == sample)[0];
-      console.log(filteredData);
-      var demoInfo = d3.select("#sample-metadata");
+      var metadataFiltered = metadata.filter(d =>d.id == sample)[0];
+      console.log(metadataFiltered);
+      var graphData = d3.select("#sample-metadata");
       // clearing previous data
-      demoInfo.html("");
-      
+      graphData.html("");
+      graphData.append("h5").text(`id: ${metadataFiltered.id}`);
+      graphData.append("h5").text(`ethnicity: ${metadataFiltered.ethnicity}`);
+      graphData.append("h5").text(`gender: ${metadataFiltered.gender}`);
+      graphData.append("h5").text(`age: ${metadataFiltered.age}`);
+      graphData.append("h5").text(`location: ${metadataFiltered.location}`);
+      graphData.append("h5").text(`bbtype: ${metadataFiltered.bbtype}`);
+      graphData.append("h5").text(`wfreq: ${metadataFiltered.wfreq}`);
+      // Object.entries(metadataFiltered).forEach((key) => {graphData.append("h5").text});
+  });
+
+};
