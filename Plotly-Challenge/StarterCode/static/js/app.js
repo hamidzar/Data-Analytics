a77eabc98c8db20202c3c806186a2e6375cb704c
hamidzar <hamid.zarringhalam@gmail.com>
2021-01-23 18:04:45 -0500

    Update app.js

--- Plotly-Challenge/StarterCode/static/js/app.js
+++ Plotly-Challenge/StarterCode/static/js/app.js
@@ -22,42 +22,71 @@ function Main(sample){
   };
   
   function plots(sample){
-    d3.json("samples.json").then((data) => {
-        console.log(data)
-  
-        var metadata = data.samples;
-        var metadataFiltered = metadata.filter(d =>d.id == sample)[0];
-        var otuID = metadataFiltered.otu_ids.slice(0, 10).reverse() ;
-        console.log(otuID)
-        var otuString= otuID.map(d => `otu ${d}`)
-        console.log(otuString)
-        var Label= metadataFiltered.otu_labels.slice(0, 10);
-        var Values = metadataFiltered.sample_values.slice(0, 10).reverse();
-  
-        // Preparing the Chart for bar Chart
-        var trace1 = {
-            x: Values,
-            y: otuString,
-            text: Label,
-            type:"bar",
-            orientation: "h",
-            };
+      d3.json("samples.json").then((data) => {
+          console.log(data)
+    
+        //  otu_ids for the x values.
+        //  sample_values for the y values.
+        //  sample_values for the marker size.
+        //  otu_ids for the marker colors.
+        //  otu_labels for the text values
+          var metadata = data.samples;
+          var metadataFiltered = metadata.filter(d =>d.id == sample)[0];
+          var otuID = metadataFiltered.otu_ids.slice(0, 10).reverse() ;
+          console.log(otuID)
+          var otuString= otuID.map(d => `otu ${d}`)
+          console.log(otuString)
+          var Label= metadataFiltered.otu_labels.slice(0, 10);
+          var Values = metadataFiltered.sample_values.slice(0, 10).reverse();
+    
+          // Preparing the Chart for bar Chart
 
-        // Data array for the plot
-        var data1 = [trace1];
-        // Setting the Layout variable for plots layout
-        var layout = {
-            title: "Top 10 OTU",
-            yaxis:{
-                tickmode:"linear",
-            },
-            margin: {
-                l: 100,
-                r: 100,
-                t: 30,
-                b: 20
-            }
-        };
-                    
-        // Ploting the chart with a div tag and id "bar"
-        Plotly.newPlot("bar", data1, layout);
+          var trace1 = {
+              x: Values,
+              y: otuString,
+              text: Label,
+              type:"bar",
+              orientation: "h",
+              };
+
+          // Data array for the plot
+          var data1 = [trace1];
+    
+          // Setting the Layout variable for plots layout
+          var layout = {
+              title: "Top 10 OTU",
+              yaxis:{
+                  tickmode:"linear",
+              },
+              margin: {
+                  l: 100,
+                  r: 100,
+                  t: 30,
+                  b: 20
+              }
+          };
+                      
+          // Ploting the chart with a div tag and id "bar"
+          Plotly.newPlot("bar", data1, layout);
+    
+          // Creating Bubble chart
+          var trace2 = {
+              x: otuID,
+              y: Values,
+              mode: "markers",
+              marker: {
+                  size: Values,
+                  color: otuID
+              },
+              text: Label
+    
+          };
+    
+          // Setting the layout for the bubble plot
+          var layout = {
+              xaxis:{title: "OTU ID"},
+              height: 600,
+              width: 1300
+          };
+    
+          
