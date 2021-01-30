03d6af26a6b776c3dc3aad7fe6a89da7e1efb47c
hamidzar <hamid.zarringhalam@gmail.com>
2021-01-29 19:07:59 -0500

    Update app.js

--- D3-Challenge/D3_data_journalism/assets/js/app.js
+++ D3-Challenge/D3_data_journalism/assets/js/app.js
@@ -12,3 +12,8 @@ var margin = {
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
+// Create an SVG wrapper
+var svg = d3.select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
