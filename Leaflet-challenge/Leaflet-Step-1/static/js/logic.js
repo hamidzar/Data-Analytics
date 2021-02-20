9c53374ae9de5bf07e3e04767d0536f1705f3d70
hamidzar <hamid.zarringhalam@gmail.com>
2021-02-19 19:54:44 -0500

    Leaflet Challenge

---
+++ Leaflet-challenge/Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,14 @@
+var myMap = L.map("map", {
+  center: [37.8968, -119.5828],
+  zoom: 3.5,
+  });
+console.log("Welcome to Map Creation")
+
+// Title layer
+L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+}).addTo(myMap);
+
